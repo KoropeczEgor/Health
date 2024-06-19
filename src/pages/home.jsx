23e8f89cb17fd9291f01a6a62c6ca7beb70f2f04
Egor <koropeczegor@gmail.com>
2024-06-19 17:45:50 +0300

    Merge pull request#4 create Hero

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/navBar";
+import Hero from "../components/hero";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
+      <Hero />
     </div>
   );
 };
