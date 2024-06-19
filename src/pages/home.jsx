35b526c8557e2ad62a9f055eaccdf144518904fd
Egor <koropeczegor@gmail.com>
2024-06-19 18:06:33 +0300

    Merge pull request#5 creat info

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../components/navBar";
 import Hero from "../components/hero";
+import Info from "../components/info/info";
 
 const Home = () => {
   return (
     <div>
       <NavBar />
       <Hero />
+      <Info />
     </div>
   );
 };
