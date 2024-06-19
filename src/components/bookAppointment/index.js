22d48c8a5107a8b677b94a943a03937d6d1853e6
Egor <koropeczegor@gmail.com>
2024-06-19 21:42:20 +0300

    Merge pull request#8 creat BookAppointment

---
+++ src/components/bookAppointment/index.js
@@ -0,0 +1,3 @@
+import BookAppointment from "./bookAppointment";
+
+export default BookAppointment;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import NavBar from "../components/navBar";
 import Hero from "../components/hero";
 import Info from "../components/info/info";
+import About from "../components/about/about";
+import BookAppointment from "../components/bookAppointment";
 
 const Home = () => {
   return (
@@ -9,6 +11,8 @@ const Home = () => {
       <NavBar />
       <Hero />
       <Info />
+      <About />
+      <BookAppointment />
     </div>
   );
 };
